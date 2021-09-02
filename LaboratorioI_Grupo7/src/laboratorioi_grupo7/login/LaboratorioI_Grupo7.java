18715319e33ce574f2ad8f34031524799e245931
nm000 <79460564+nm000@users.noreply.github.com>
2021-09-02 16:48:52 -0500

    creado

---
+++ LaboratorioI_Grupo7/src/laboratorioi_grupo7/login/LaboratorioI_Grupo7.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorioi_grupo7.login;
+
+/**
+ *
+ * @author 57301
+ */
+public class LaboratorioI_Grupo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        
+    }
+    
+}
